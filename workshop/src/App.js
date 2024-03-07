80a86cc769a9cc1b17f6a45b805f1cf69fea94b4
igorSguissardi <159180268+igorSguissardi@users.noreply.github.com>
2024-03-07 17:54:26 -0300

    dia1 - React Blockchain
    ...

---
+++ workshop/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>className+
+      <h1>Hello, world!</h1>
+      <p>Bahhhhh, é os Guri</p>
+      </div>
+  );
+}
+
+export default App;
