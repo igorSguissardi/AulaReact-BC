d84dead6ef1e8cd3e2701f8f7af90f8f20cccea4
igorSguissardi <159180268+igorSguissardi@users.noreply.github.com>
2024-03-18 15:54:38 -0300

    Update App.js

--- workshop/src/App.js
+++ workshop/src/App.js
@@ -6,15 +6,15 @@ function App() {
       <Input description="Digite o seu e-mail" label="E-mail" placeholder="igor@gmail.com" type="text"/>
       <Input description="Carteira para receber NFT" label="Wallet" placeholder="00.00...0000" type="text"/>
       <Input description="Seu nome" label="Nome" placeholder="Johndoe" type="text"/>
-    </div className="bg-red-600 w-5/6"> 
+   
       <p> receba a sua Proof-Of-Attandance NFT</p>
 
       <p className="">
-        Para receber sua NFT você precisa ter participado do evento e ter uma carteira na rede sepolia
+        {/* Para receber sua NFT você precisa ter participado do evento e ter uma carteira na rede sepolia */}
       </p>
-
-      // o próximo significa que ele vai colocar os itens em coluna -> para centralizar é necessário colocar em dentro da div
-  );
+      {/* // o próximo significa que ele vai colocar os itens em coluna -> para centralizar é necessário colocar em dentro da div */}
+    </div> 
+  )
 }
 
 export default App;
