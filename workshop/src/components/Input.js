a50e9db0b008c28b0241a88de68364b30e550447
igorSguissardi <159180268+igorSguissardi@users.noreply.github.com>
2024-03-19 14:03:23 -0300

    Finalizado

--- workshop/src/components/Input.js
+++ workshop/src/components/Input.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-// props são argumentos que podem ser passados para um componente React para personalizar sua aparência ou comportamento.
+// props: Props são argumentos que podem ser passados para um componente React para personalizar sua aparência ou comportamento.
 export default function Input(props) {
-    return ( 
-        <div className="flex flox-col mb-2">
-            <label htmlFor="name">{props.name}</label>
-            <span className="text-sm text-gray-200">
-                {props.description}
-            </span>
-            <input 
-                id="name"
-                type="text"
-                placeholder={props.placeholder}
-                className="p-2 text-black rounded-md shadow-md"
-            />
-        </div>
-    );
+  return (
+    <div className="flex flex-col mb-2">
+      <label htmlFor="name">{props.name}</label>
+      <span className="text-sm text-gray-200">
+        {props.description}
+      </span>
+      <input
+        id="name"
+        type="text"
+        placeholder={props.placeholder}
+        className="p-2 text-black rounded-md shadow-md"
+      />
+    </div>
+  );
 }
